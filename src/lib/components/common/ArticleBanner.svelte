a0f30cbfe31fba3c95a845c0ad3f3b111c7802cd
Riley1101 <www.riley.s.eileen@gmail.com>
2023-06-01 22:37:08 -0400

    added detail page components for block contents

---
+++ src/lib/components/common/ArticleBanner.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { Button, ButtonSet } from 'carbon-components-svelte';
+	import { Share } from 'carbon-icons-svelte';
+</script>
+
+<div
+	class="w-full bg-gradient-to-b from-black to-gray-900 text-white min-h-[300px] flex justify-end flex-col"
+>
+	<div class="section-container box w-full">
+		<h1 class="text-3xl lg:text-5xl lg:leading-[1.2] my-6">
+			Moving forward: On deprecating carbon components and carbon components react
+		</h1>
+		<div class="flex items-center gap-4">
+			<div class="w-[60px] aspect-square rounded-full bg-gray-800" />
+			<div class="flex flex-col">
+				<span class="uppercase mb-1 text-sm"> sai mawn khan </span>
+				<div class="flex items-center gap-4">
+					<span class="text-gray-500">5 min read</span>
+					<span class="text-sm text-gray-500"> Jan 12, 2023 </span>
+				</div>
+			</div>
+		</div>
+		<!--
+		<ButtonSet class="mt-8 flex gap-1 bg-transparent max-w-max">
+			<Button icon={Share} class="bg-gray-900 text-gray-300 ">Facebook</Button>
+			<Button icon={Share} class="bg-gray-900 text-gray-300">Twitter</Button>
+			<Button icon={Share} size="field" class="bg-gray-900 text-gray-300">LinkedIn</Button>
+		</ButtonSet>
+        -->
+	</div>
+</div>
---
+++ src/routes/news/[slug]/+page.svelte
@@ -0,0 +1,124 @@
+<script>
+	import ArticleBanner from '$lib/components/common/ArticleBanner.svelte';
+	import {
+		Link,
+		OutboundLink,
+		CodeSnippet,
+		OrderedList,
+		ListItem,
+		UnorderedList
+	} from 'carbon-components-svelte';
+	import { Link as LinkIcon } from 'carbon-icons-svelte';
+	const data = [1];
+</script>
+
+<div class="min-h-screen">
+	<ArticleBanner />
+	<div class="p-4 md:p-12 max-w-[1000px] mx-auto">
+		<h2 class="text-xl md:text-3xl mb-2 md:mb-4 font-bold">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, quas?
+		</h2>
+
+		{#each data as item}
+			<p class="text-base md:text-lg md:leading-[1.8] my-2">
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+				Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+				assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+				Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+				debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+				Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+				necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam
+				eligendi blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque
+				porro! Recusandae explicabo consequuntur quis?
+			</p>
+		{/each}
+		<h3 class="text-lg md:text-2xl mt-6 mb-2 font-bold">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, quas?
+		</h3>
+		<p class="text-base md:text-lg md:leading-[1.8] my-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+			Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+			assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+			Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+			debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+			Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+			necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam
+			eligendi blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque porro!
+			Recusandae explicabo consequuntur quis?
+		</p>
+		<h4 class="text-base md:text-xl mt-6 mb-2 font-bold">
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi, quas?
+		</h4>
+		<p class="text-base md:text-lg md:leading-[1.8] my-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+			Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+			assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+			Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+			debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+			Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+			necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam
+			eligendi blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque porro!
+			Recusandae explicabo consequuntur quis?
+		</p>
+		<Link href="https://www.carbondesignsystem.com/" icon={LinkIcon} class="mt-2"
+			>Carbon Design System</Link
+		>
+		<p class="text-base md:text-lg md:leading-[1.8] my-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+			Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+			assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+			Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+			debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+			Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+			necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam
+			eligendi blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque porro!
+			Recusandae explicabo consequuntur quis?
+		</p>
+		<OrderedList class="pl-16 mt-2 text-lg">
+			<ListItem>Ordered list item</ListItem>
+			<ListItem>Ordered list item</ListItem>
+			<ListItem>Ordered list item</ListItem>
+		</OrderedList>
+		<p class="text-base md:text-lg md:leading-[1.8] my-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+			Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+			assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+			Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+			debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+			Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+			necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam
+			eligendi blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque porro!
+			Recusandae explicabo consequuntur quis?
+		</p>
+		<UnorderedList class="pl-16 mt-2 text-lg">
+			<ListItem>Unordered list item</ListItem>
+			<ListItem>Unordered list item</ListItem>
+			<ListItem>Unordered list item</ListItem>
+		</UnorderedList>
+		<p class="text-base md:text-lg md:leading-[1.8] my-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+			Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+			assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+			Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+			debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+			Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+			<i class="italic"> i am italic </i>
+			necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam eligendi
+			blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque porro! Recusandae
+			explicabo consequuntur quis?
+		</p>
+		<CodeSnippet code="yarn add -D carbon-components-svelte" class="my-6" />
+		<p class="text-base md:text-lg md:leading-[1.8] my-2">
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate iusto aperiam quae.
+			Impedit praesentium omnis nulla, deleniti sapiente dolore, excepturi dolor cupiditate totam,
+			assumenda corrupti atque eligendi in illum iste fugit maiores debitis quibusdam quaerat qui.
+			Omnis recusandae, cumque quas ullam nisi, eveniet ducimus maxime nihil fugit beatae autem
+			debitis cum, iure unde vitae delectus adipisci ab nobis accusamus dolores rem quidem porro?
+			Blanditiis doloremque commodi sint deleniti veniam cupiditate fuga alias? Expedita dicta,
+			<strong> i am strong </strong>
+			necessitatibus, optio veritatis voluptatum voluptatibus eaque molestiae quo fuga magnam eligendi
+			blanditiis, esse eum neque atque asperiores facere fugit quibusdam doloremque porro! Recusandae
+			explicabo consequuntur quis?
+		</p>
+	</div>
+</div>
