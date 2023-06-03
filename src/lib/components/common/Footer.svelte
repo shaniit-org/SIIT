703018ae656671079a610f5b62ce2287b52d9b77
Riley1101 <www.riley.s.eileen@gmail.com>
2023-06-02 22:19:33 -0400

    changed footer design

--- src/lib/components/common/Footer.svelte
+++ src/lib/components/common/Footer.svelte
@@ -1,18 +1,19 @@
 <script>
-	import { ClickableTile } from 'carbon-components-svelte';
 	import { navs } from '$lib/site/nav';
 </script>
 
 <footer class="bg-theme-dark">
+	<!--
 	<div class="flex justify-between items-center">
 		<span class="text-white font-bold p-4">LOGO</span>
-		<ClickableTile class="text-lg p-12 ml-auto flex flex-col">
+		<div class="text-lg p-8 md:p-12 ml-auto flex flex-col bg-theme-muted">
 			<span class="text-xl text-theme-light">Contact Now !</span>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nesciunt!
-		</ClickableTile>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, nesciunt!</p>
+		</div>
 	</div>
+    -->
 	<div class="flex items-center p-4">
-		<ul class="flex text-white gap-x-10">
+		<ul class="flex flex-wrap gap-y-4 justify-center text-white gap-x-10">
 			{#each navs as nav}
 				<li>
 					<a href={nav.path} class="text-theme-light hover:text-blue-500">{nav.name}</a>
