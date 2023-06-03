78fed82b2d55a30f73ce35421caca2d19c286203
Riley1101 <www.riley.s.eileen@gmail.com>
2023-06-02 22:06:09 -0400

    added event cards and event detail page

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import SubHero from '$lib/components/common/SubHero.svelte';
+	import EventCard from '$lib/components/common/EventCard.svelte';
+	import { Select, SelectItem } from 'carbon-components-svelte';
+	const data = [1, 2, 3];
+</script>
+
+<div class="min-h-screen">
+	<SubHero title="Events" />
+	<div class="section-container box mb-12">
+		<div class="mb-8">
+			<Select labelText="Sorted by" class="max-w-max ml-auto min-w-[200px]">
+				<SelectItem value="white" />
+				<SelectItem value="g10" />
+				<SelectItem value="g80" />
+				<SelectItem value="g90" />
+				<SelectItem value="g100" />
+			</Select>
+		</div>
+		<div class="flex flex-col gap-12">
+			{#each data as item}
+				<EventCard />
+			{/each}
+		</div>
+	</div>
+</div>
