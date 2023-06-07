14685b90b528bea09a2db2193751aad6bbb1bb7a
Riley1101 <www.riley.s.eileen@gmail.com>
2023-06-06 23:23:45 -0400

    redced lineheight in header

--- src/lib/components/common/SubHero.svelte
+++ src/lib/components/common/SubHero.svelte
@@ -9,6 +9,6 @@
 	class="w-full bg-gradient-to-b from-black to-gray-900 text-white min-h-[300px] flex justify-end flex-col"
 >
 	<div class="section-container border p-12 w-full">
-		<h1 class="text-6xl mb-4">{title}</h1>
+		<h1 class="text-5xl leading-[1.1] md:text-6xl mb-4">{title}</h1>
 	</div>
 </div>
