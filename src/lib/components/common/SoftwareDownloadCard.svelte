bad20c10a7170afe6160768d1fe55f9ff409d55f
Riley1101 <www.riley.s.eileen@gmail.com>
2023-06-06 22:29:01 -0400

    added fonts and keyboards

---
+++ src/lib/components/common/SoftwareDownloadCard.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Button, Tag } from 'carbon-components-svelte';
+	import { Download } from 'carbon-icons-svelte';
+</script>
+
+<div class="bg-white">
+	<div class="relative">
+		<div class="absolute top-0 right-0">
+			<Button
+				size="lg"
+				icon={Download}
+				aria-label="Download"
+				iconDescription="Download"
+				class="bg-theme-dark"
+			/>
+		</div>
+	</div>
+	<div class="flex flex-col p-4">
+		<h2 class="text-lg mb-1">Ubuntu for You</h2>
+		<div class="mb-2">
+			<Tag type="blue" size="sm" class="m-0">Keyboard</Tag>
+		</div>
+		<p class="text-sm">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero exercitationem reprehenderit
+			sed delectus! Delectus provident ipsa et quos eos obcaecati!
+		</p>
+	</div>
+</div>
--- src/routes/fonts/+page.svelte
+++ src/routes/fonts/+page.svelte
@@ -1,11 +1,31 @@
 <script>
-	import { ClickableTile } from 'carbon-components-svelte';
+	import Mac from '$lib/components/icons/mac.svelte';
+	import Android from '$lib/components/icons/android.svelte';
+	import Windows from '$lib/components/icons/windows.svelte';
+	import { ContentSwitcher, Switch } from 'carbon-components-svelte';
 	import SubHero from '$lib/components/common/SubHero.svelte';
+	import { TextFont, Keyboard } from 'carbon-icons-svelte';
+	import SoftwareDownloadCard from '$lib/components/common/SoftwareDownloadCard.svelte';
+	import Linux from '$lib/components/icons/linux.svelte';
 </script>
 
 <SubHero title="Fonts & Keyboards" />
 <div class="min-h-screen section-container box">
-	<!--
+	<div class="mb-6">
+		<ContentSwitcher>
+			<Switch text="All" />
+			<Switch>
+				<div style="display: flex; align-items: center;">
+					<Keyboard style="margin-right: 0.5rem;" />Keyboard
+				</div>
+			</Switch>
+			<Switch>
+				<div style="display: flex; align-items: center;">
+					<TextFont style="margin-right: 0.5rem;" />Font
+				</div>
+			</Switch>
+		</ContentSwitcher>
+	</div>
 	<div class="flex flex-col gap-2">
 		<p class="text-xl leading-relaxed">
 			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quis quos provident quaerat
@@ -13,11 +33,60 @@
 			ipsam, dolorum aspernatur quis quaerat.
 		</p>
 	</div>
-	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-12">
-		<div class="flex flex-col gap-2">
-			<h2 class="text-lg font-bold">Linux</h2>
-			<ClickableTile href="https://www.carbondesignsystem.com/" />
+	<div class="mt-12">
+		<div class="flex items-center gap-2">
+			<Linux />
+			<h2 class="text-xl my-4 text-theme-dark cursor-pointer">Linux</h2>
+		</div>
+		<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
 		</div>
 	</div>
-    -->
+	<div class="mt-12">
+		<div class="flex items-center gap-2">
+			<Windows />
+			<h2 class="text-xl my-4 text-theme-dark cursor-pointer">Window</h2>
+		</div>
+		<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+		</div>
+	</div>
+	<div class="mt-12">
+		<div class="flex items-center gap-2">
+			<Mac />
+			<h2 class="text-xl my-4 text-theme-dark cursor-pointer">Mac</h2>
+		</div>
+		<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+		</div>
+	</div>
+
+	<div class="mt-12">
+		<div class="flex items-center gap-2">
+			<Android />
+			<h2 class="text-xl my-4 text-theme-dark cursor-pointer">Android</h2>
+		</div>
+		<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+		</div>
+	</div>
+	<div class="mt-12">
+		<div class="flex items-center gap-2">
+			<Mac />
+			<h2 class="text-xl my-4 text-theme-dark cursor-pointer">IOS</h2>
+		</div>
+		<div class="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+			<SoftwareDownloadCard />
+		</div>
+	</div>
 </div>
