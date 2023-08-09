e48e870612ad11936d1138ab067d3cb5b1bb5164
Riley1101 <arkardev00@gmail.com>
2023-08-08 23:54:33 -0400

    change size in related article cards

--- src/lib/components/common/RelatedArticle.svelte
+++ src/lib/components/common/RelatedArticle.svelte
@@ -7,7 +7,7 @@
 {#if data.length > 0}
 	<div class="section-container p-4 md:px-12">
 		<h3 class="text-xl mb-4">Related Articles</h3>
-		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pb-12">
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-12">
 			{#each data as itm}
 				<NewsCard data={itm} />
 			{/each}
