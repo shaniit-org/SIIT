94069d6ba62a69c47767bc2e062bacbf9c4f5082
Riley1101 <arkardev00@gmail.com>
2023-10-02 21:22:59 -0400

    added

--- src/lib/components/common/FloatingDonate.svelte
+++ src/lib/components/common/FloatingDonate.svelte
@@ -2,6 +2,7 @@
 	import { fade } from 'svelte/transition';
 	import { onMount } from 'svelte';
 	import { ClickableTile, ImageLoader, Button } from 'carbon-components-svelte';
+	import CloseOutline from 'carbon-icons-svelte/lib/CloseOutline.svelte';
 
 	let show = false;
 	onMount(() => {
@@ -15,14 +16,22 @@
 </script>
 
 {#if show}
-	<div in:fade={{ duration: 500 }} out:fade={{ duration: 200 }}>
+	<div in:fade={{ duration: 500 }} out:fade={{ duration: 200 }} class="relative">
 		<ClickableTile
-			on:click={close}
 			style="padding-inline: 1.5rem; padding-block: 1.5rem;"
-			href="/donate"
 			class="translate-hover w-[250px]  hover:bg-white text-center hover:scale-[1.04] duration-500 
-    fixed top-[30%] right-10 shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] flex flex-col gap-4  "
+    fixed top-[30%] right-5 md:right-10 shadow-[rgba(0,_0,_0,_0.2)_0px_60px_40px_-7px] flex flex-col gap-4  "
 		>
+			<Button
+				tooltipPosition="left"
+				tooltipAlignment="center"
+				on:click={close}
+				iconDescription="Close"
+				icon={CloseOutline}
+				style="position: absolute; top: 0; right: 0;"
+				class="bg-transparent text-black hover:bg-red-600 transition-all duration-500 max-w-max"
+				size="lg"
+			/>
 			<h2 class="leading-[1.2] flex items-center gap-1">
 				<span class="w-1/2 h-[2px] bg-[#E3983B]" />
 				<span class="w-full text-base font-semibold"> Make Some Donation </span>
