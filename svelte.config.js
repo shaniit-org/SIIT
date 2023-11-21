3d3cef354bf812acfa01c39d17a7f030db74a24b
Riley1101 <arkardev00@gmail.com>
2023-11-20 23:16:32 -0500

    update on adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
 import { optimizeImports } from 'carbon-preprocess-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
 import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: [
-        sveltePreprocess({
-            scss: true
-        }),
-        optimizeImports()
-    ],
-    kit: {
-        adapter: adapter()
-    }
+	preprocess: [
+		sveltePreprocess({
+			scss: true
+		}),
+		optimizeImports()
+	],
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		})
+	}
 };
 
 export default config;
