d5c99844b3904a49d16542f5b61c5fb818143ecf
Riley1101 <arkardev00@gmail.com>
2023-11-20 23:09:41 -0500

    update on deps

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import { optimizeImports } from 'carbon-preprocess-svelte';
 const ENV = process.env.NODE_ENV;
 
 export default defineConfig({
-	plugins: [
-		sveltekit({
-			preprocess: [optimizeImports()]
-		})
-	],
-	ssr: {
-		noExternal: ENV === 'production' ? ['instantsearch.js', 'algoliasearch', '@carbon/charts'] : []
-	}
+    plugins: [
+        sveltekit({
+            preprocess: [optimizeImports()]
+        })
+    ],
+    ssr: {
+        noExternal: ENV === 'production' ? ['instantsearch.js', 'algoliasearch', '@carbon/charts'] : []
+    }
 });
