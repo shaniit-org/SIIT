cb3b6dbcd60d98cbba587c7f161b5131acc7ff52
Riley1101 <arkardev00@gmail.com>
2023-11-22 18:03:51 -0500

    update on removing polyfils

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { optimizeImports } from 'carbon-preprocess-svelte';
-
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 const ENV = process.env.NODE_ENV;
 
 export default defineConfig({
 	plugins: [
 		sveltekit({
 			preprocess: [optimizeImports()]
-		}),
-		nodePolyfills()
+		})
 	],
 	ssr: {
 		noExternal: ENV === 'production' ? ['instantsearch.js', 'algoliasearch', '@carbon/charts'] : []
