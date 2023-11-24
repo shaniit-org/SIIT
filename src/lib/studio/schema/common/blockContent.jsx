9a4e689b70aef3857b5e0a1051e166862b8aaa34
Riley1101 <arkardev00@gmail.com>
2023-11-24 11:17:14 -0500

    update on precommit and formatting

--- src/lib/studio/schema/common/blockContent.jsx
+++ src/lib/studio/schema/common/blockContent.jsx
@@ -1,80 +1,73 @@
 import { defineArrayMember, defineType } from 'sanity';
-import React from 'react';
-
-const FontSizeIcon = () => <span style={{ fontWeight: 'bold' }}>T</span>;
-
-const FontSizeComponent = (props) => (
-    <span style={{ backgroundColor: 'yellow' }}>{props.children}</span>
-);
 
 export default defineType({
-    title: 'Block Content',
-    name: 'blockContent',
-    type: 'array',
-    of: [
-        defineArrayMember({
-            type: 'code',
-            title: 'Code',
-            options: {
-                theme: 'github',
-                darkTheme: 'terminal'
-            }
-        }),
-        defineArrayMember({
-            title: 'Image',
-            type: 'image',
-            fields: [
-                {
-                    name: 'alt',
-                    type: 'string',
-                    title: 'Alternative text'
-                }
-            ]
-        }),
-        defineArrayMember({
-            title: 'Block',
-            type: 'block',
-            // Styles let you set what your user can mark up blocks with. These
-            // correspond with HTML tags, but you can set any title or value
-            // you want and decide how you want to deal with it where you want to
-            // use your content.
-            styles: [
-                { title: 'Normal', value: 'normal' },
-                { title: 'H1', value: 'h1' },
-                { title: 'H2', value: 'h2' },
-                { title: 'H3', value: 'h3' },
-                { title: 'H4', value: 'h4' },
-                { title: 'Quote', value: 'blockquote' }
-            ],
-            lists: [
-                { title: 'Bullet', value: 'bullet' },
-                { title: 'Numbered', value: 'number' }
-            ],
+	title: 'Block Content',
+	name: 'blockContent',
+	type: 'array',
+	of: [
+		defineArrayMember({
+			type: 'code',
+			title: 'Code',
+			options: {
+				theme: 'github',
+				darkTheme: 'terminal'
+			}
+		}),
+		defineArrayMember({
+			title: 'Image',
+			type: 'image',
+			fields: [
+				{
+					name: 'alt',
+					type: 'string',
+					title: 'Alternative text'
+				}
+			]
+		}),
+		defineArrayMember({
+			title: 'Block',
+			type: 'block',
+			// Styles let you set what your user can mark up blocks with. These
+			// correspond with HTML tags, but you can set any title or value
+			// you want and decide how you want to deal with it where you want to
+			// use your content.
+			styles: [
+				{ title: 'Normal', value: 'normal' },
+				{ title: 'H1', value: 'h1' },
+				{ title: 'H2', value: 'h2' },
+				{ title: 'H3', value: 'h3' },
+				{ title: 'H4', value: 'h4' },
+				{ title: 'Quote', value: 'blockquote' }
+			],
+			lists: [
+				{ title: 'Bullet', value: 'bullet' },
+				{ title: 'Numbered', value: 'number' }
+			],
 
-            // Marks let you mark up inline text in the block editor.
-            marks: {
-                // Decorators usually describe a single property – e.g. a typographic
-                // preference or highlighting by editors.
-                decorators: [
-                    { title: 'Strong', value: 'strong' },
-                    { title: 'Emphasis', value: 'em' }
-                ],
-                // Annotations can be any object structure – e.g. a link or a footnote.
-                annotations: [
-                    {
-                        title: 'URL',
-                        name: 'link',
-                        type: 'object',
-                        fields: [
-                            {
-                                title: 'URL',
-                                name: 'href',
-                                type: 'url'
-                            }
-                        ]
-                    }
-                ]
-            }
-        })
-    ]
+			// Marks let you mark up inline text in the block editor.
+			marks: {
+				// Decorators usually describe a single property – e.g. a typographic
+				// preference or highlighting by editors.
+				decorators: [
+					{ title: 'Strong', value: 'strong' },
+					{ title: 'Emphasis', value: 'em' }
+				],
+				// Annotations can be any object structure – e.g. a link or a footnote.
+				annotations: [
+					{
+						title: 'URL',
+						name: 'link',
+						type: 'object',
+						fields: [
+							{
+								title: 'URL',
+								name: 'href',
+								type: 'url'
+							}
+						]
+					}
+				]
+			}
+		})
+	]
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./src/**/**/*.{html,js,svelte,ts}'],
-    theme: {
-        fontFamily: {
-            san: ['GreatHorKham Yangon', 'sans-serif']
-        },
-        extend: {
-            colors: {
-                'theme-primary': '#D0E2FF',
-                'theme-dark': '#161616',
-                'theme-muted': '#e8e8e8',
-                'theme-secondary': '#E8DAFF'
-            }
-        }
-    },
-    plugins: [require('@tailwindcss/typography')]
+	content: ['./src/**/**/*.{html,js,svelte,ts}'],
+	theme: {
+		fontFamily: {
+			san: ['GreatHorKham Yangon', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				'theme-primary': '#D0E2FF',
+				'theme-dark': '#161616',
+				'theme-muted': '#e8e8e8',
+				'theme-secondary': '#E8DAFF'
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
 };
