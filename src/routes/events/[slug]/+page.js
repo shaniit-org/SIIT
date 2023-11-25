d5e2da918eac26dafebc8c7e7b13f9408695f95e
Riley1101 <arkardev00@gmail.com>
2023-11-25 13:17:43 -0500

    update on related details issue

--- src/routes/events/[slug]/+page.js
+++ src/routes/events/[slug]/+page.js
@@ -4,6 +4,7 @@ import { getPageData } from '$lib/sanity/query';
 const query = `*[_type =="event" && slug.current==$slug][0]{
     ...,
       "related": *[_type == "event" && _id != ^._id && count(categories[@._ref in ^.^.categories[]._ref]) > 0] | order(publishedAt desc, _createdAt desc) [0..2] {
+          _type,
          title,
             coverImage,
          _id,
@@ -16,16 +17,16 @@ const query = `*[_type =="event" && slug.current==$slug][0]{
  * @type {import('@sveltejs/kit').Load}
  */
 export const load = async ({ params }) => {
-	/** @type {string} */
-	if (params.slug) {
-		/**
-		 * @type {import('$lib/types').EventDetail}
-		 */
-		const data = await getPageData(query, {
-			slug: params.slug
-		});
-		if (data) return { data };
-		else throw error(404, 'not found');
-	}
-	throw error(404, 'not found');
+    /** @type {string} */
+    if (params.slug) {
+        /**
+         * @type {import('$lib/types').EventDetail}
+         */
+        const data = await getPageData(query, {
+            slug: params.slug
+        });
+        if (data) return { data };
+        else throw error(404, 'not found');
+    }
+    throw error(404, 'not found');
 };
