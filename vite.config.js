349a3b690638eae41e42a92e6821d45fc4118b79
Riley1101 <arkardev00@gmail.com>
2023-12-07 10:26:23 -0500

    remve highlight js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { optimizeImports } from 'carbon-preprocess-svelte';
+
 const ENV = process.env.NODE_ENV;
 
 export default defineConfig({
-	plugins: [
-		sveltekit({
-			preprocess: [optimizeImports()]
-		})
-	],
+	plugins: [sveltekit()],
 	ssr: {
 		noExternal: ENV === 'production' ? ['instantsearch.js', 'algoliasearch', '@carbon/charts'] : []
 	}
