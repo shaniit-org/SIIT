985dbf813735556c3097b665efc9b3f31b774a7d
Riley1101 <arkardev00@gmail.com>
2024-01-26 16:57:57 -0500

    testing on netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
 import { optimizeImports } from 'carbon-preprocess-svelte';
 import sveltePreprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		sveltePreprocess({
-			scss: true,
-			preserve: ['ld+json']
-		}),
-		optimizeImports()
-	],
-	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
-	}
+    preprocess: [
+        sveltePreprocess({
+            scss: true,
+            preserve: ['ld+json']
+        }),
+        optimizeImports()
+    ],
+    kit: {
+        adapter: adapter({
+            // See below for an explanation of these options
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+    }
 };
 
 export default config;
