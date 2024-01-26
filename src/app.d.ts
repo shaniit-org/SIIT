3bbb250f3502b0d88f48459b593d635ab6a74cf4
Riley1101 <arkardev00@gmail.com>
2024-01-26 11:23:25 -0500

    added env types

--- src/app.d.ts
+++ src/app.d.ts
@@ -42,6 +42,15 @@ declare global {
 		// interface Locals {}
 		// interface PageData {}
 		// interface Platform {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
 	}
 }
 declare module 'svelte-share-buttons-component' {}
