f05e12a913eb8830c6fec8f1fcb7deaf2df747ad
Riley1101 <arkardev00@gmail.com>
2024-03-03 10:59:40 -0500

    wip- term refactor to catalog

--- src/routes/terms/[chars]/+page.svelte
+++ src/routes/terms/[chars]/+page.svelte
@@ -12,9 +12,10 @@
 <div class="min-h-screen">
 	<SubHero title="Terms" />
 	<div class="section-container box">
+        <h1 class="h1">Chars</h1>
 		<div class="flex flex-wrap gap-4 mt-12">
 			{#each alphabets as alphabet}
-				<a href="/terms/{alphabet}" class=" basis-[20%] md:basis-[10%] card hover:card-hover p-4 md:p-8 grid place-items-center  scroll-mt-24" id={alphabet}>
+				<a href="/terms/chars/{alphabet}" class=" basis-[20%] md:basis-[10%] card hover:card-hover p-4 md:p-8 grid place-items-center  scroll-mt-24" id={alphabet}>
 					<h2 class="h2 font-bold ">{alphabet}</h2>
 				</a>
 			{/each}
